ac05cf82e7c5a78bbf989f939696d0e0ea025a95
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-11-19 12:19:59 +0530

    Added Components folder , Layout folder and pages folder which has Login , signup, adminDashboard, All Blogs etc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/page";
+import MainLayout from "./layout/MainLayout";
+import OtherLayout from "./layout/OtherLayout";
+import Login from "./pages/Login/page";
+import SignUp from "./pages/SignUp/page";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="px-64">
+      {/* padding from x axis is 64 */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+          </Route>
 
-export default App
+          <Route element={<OtherLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/layout/MainLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer/Footer'
+
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Navbar/>
+        <main>
+            <Outlet/>
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainLayout
