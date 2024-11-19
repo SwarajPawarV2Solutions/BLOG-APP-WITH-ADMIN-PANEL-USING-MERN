3d80b8badd035562dc6f15aa01958f7cff8bf5fd
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-11-19 12:35:24 +0530

    Added Profile folder in pages folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Profiler } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/page";
 import MainLayout from "./layout/MainLayout";
 import OtherLayout from "./layout/OtherLayout";
 import Login from "./pages/Login/page";
 import SignUp from "./pages/SignUp/page";
+import Profile from "./pages/Profile/page";
 const App = () => {
   return (
     <div className="px-64">
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
 
           <Route element={<OtherLayout />}>
